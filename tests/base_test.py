b1bdd21576a4250a3f3ba30c0dd695e310cb9548
nadralia <nadralia@gmail.com>
2018-11-01 20:57:07 +0300

    Writing the tests for you the methods created in this project [Deliveries #161546604]

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,17 +1,17 @@
 import unittest
 from run import app
 from flask import json
-from app.db.db_manager import DBConnection
-from app.controllers.user_controller import UserController
+from api.databases.dbconnect import Database
+from api.controllers.user import UserController
 
-connection = DBConnection()
+connection = Database()
 user_controller = UserController()
 
 class BaseTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.user_name = "admin"
-        self.contact = "0703-000000"
+        self.username = "admin"
+        self.phone = "0779-003145"
         self.role = "admin"
         self.password = "admin"
         self.app = app.test_client(self)
@@ -24,16 +24,16 @@ class BaseTestCase(unittest.TestCase):
         connection.delete_tables()
 
     def register_admin(self):
-        user_controller.add_attendant(self.user_name, self.contact, self.role, self.password)
+        user_controller.add_new_user(self.username, self.phone, self.role, self.password)
         
     def register_attendant(self):
-        user_controller.add_attendant("araali", "0703-000001", "attendant", "araali")
+        user_controller.add_new_user("nadralia", "0779-003145", "attendant", "nadra2922")
     
     def admin_login(self):
         response = self.app.post(
             "/api/auth/login",
             content_type='application/json',
-            data=json.dumps(dict(user_name=self.user_name, password=self.password))
+            data=json.dumps(dict(username=self.username, password=self.password))
         )
         reply = json.loads(response.data)
         return reply
@@ -42,7 +42,7 @@ class BaseTestCase(unittest.TestCase):
         response = self.app.post(
             "/api/auth/login",
             content_type='application/json',
-            data=json.dumps(dict(user_name="araali", password="araali"))
+            data=json.dumps(dict(username="nadralia", password="nadra2922"))
         )
         reply = json.loads(response.data)
         return reply    
